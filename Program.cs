8606b031006423410f9942dfc192bc50cfe74d17
Aszalós Evila <aszalosevi@gmail.com>
2026-02-20 19:57:29 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Program
+{
+    public class TicTacToe
+    {
+        public static void Main(string[] args)
+        {
+            string[,] field = new string[5,5];
+
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                    field[i,j] = "x";
+                }
+            }
+
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+
+                    if (j %2 != 0)
+                    {
+                        field[i,j] = " | ";
+                    }
+                    if (i%2!=0)
+                    {
+                       field[i,j] = "——"; 
+                    }
+                    Console.Write(field[i,j]);
+                }
+                Console.WriteLine();
+
+
+            }
+        }
+    }
+}
