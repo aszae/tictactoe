4e42f1723422169015dba13f4808b7bea4fc99e8
Aszalós Evila <aszalosevi@gmail.com>
2026-02-20 20:13:48 +0100

    merge: Merged master and array3 branches.

--- obj/Debug/net10.0/tictactoe.AssemblyInfo.cs
+++ obj/Debug/net10.0/tictactoe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8606b031006423410f9942dfc192bc50cfe74d17")]
 [assembly: System.Reflection.AssemblyProductAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
